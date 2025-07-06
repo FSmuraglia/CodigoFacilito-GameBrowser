e09164d0ec970c24c7bf44d127aa4be1872bf9e6
FSmuraglia <francosmuraglia@gmail.com>
2025-07-06 16:50:50 -0300

    Modelos
    Modelos creados, migrados y agregados al Django Admin Panel

--- videojuegos/admin.py
+++ videojuegos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categoria, Captura, Reseña, SolicitudVideojuego, Videojuego
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Captura)
+admin.site.register(Reseña)
+admin.site.register(SolicitudVideojuego)
+admin.site.register(Videojuego)
--- videojuegos/models.py
+++ videojuegos/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from datetime import date
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    descripcion = models.TextField()
+    fecha_creacion = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.nombre
+
+class Captura(models.Model):
+    imagen = models.ImageField(null=True, blank=True, upload_to='media/videojuegos')
+    videojuego = models.ForeignKey(
+        'Videojuego', on_delete=models.CASCADE, related_name='capturas'
+    )
+
+    def __str__(self):
+        return f"Captura de {self.videojuego.titulo}"
+
+class Reseña(models.Model):
+    videojuego = models.ForeignKey(
+        'Videojuego', on_delete=models.CASCADE, related_name='reseñas'
+    )
+    usuario = models.ForeignKey(
+        User, related_name='reseñas', on_delete=models.CASCADE
+    )
+    puntuacion = models.FloatField(
+        validators=[MinValueValidator(1.0), MaxValueValidator(5.0)]
+    )
+    comentario = models.TextField()
+    fecha_creacion = models.DateField(default=date.today)
+
+    def __str__(self):
+        return f"Reseña de {self.videojuego.titulo} por {self.usuario.username} - Puntuación: {self.puntuacion}"
+
+class SolicitudVideojuego(models.Model):
+    ESTADOS = [
+        ('PEND', 'Pendiente'),
+        ('APROB', 'Aprobada'),
+        ('RECH', 'Rechazada')
+    ]
+
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    estado = models.CharField(max_length=5, choices=ESTADOS, default='PEND')
+    usuario_solicitante = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='solicitudes'
+    )
+    usuario_aprobador = models.ForeignKey(
+        User, related_name='solicitudes_aprobadas', on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.titulo} - ({self.get_estado_display()})"
+    
+    class Meta:
+        verbose_name_plural = 'Solicitudes de Videojuegos'
+
+class Videojuego(models.Model):
+    titulo = models.CharField(max_length=100, unique=True)
+    fecha_publicacion = models.DateField(default=date.today)
+    categorias = models.ManyToManyField(Categoria, related_name='videojuegos')
+    descripcion = models.TextField()
+    usuario_creador = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='videojuegos_creados'
+    )
+
+    def __str__(self):
+        return self.titulo
